a0bb947fb735ec06eb0a35f61006dd4001ca5cf6
Liliherax <57782295+Gabriel-demian@users.noreply.github.com>
2024-07-01 21:06:52 -0300

    reviews

---
+++ js/carrouselData.js
@@ -0,0 +1,99 @@
+const carouselBoysData = [
+    {
+        src: "images/characters/Billy_Butcher.png",
+        alt: "Foto de Billy Butcher",
+        logo: "images/black_logo.png",
+        title: "Billy Butcher",
+        description: "Después de la desaparición de su esposa, Becca, Butcher dedicó su vida a luchar contra Vought y sus Super corruptos, especialmente Homelander. Butcher cruzó una línea que nunca imaginó cuando tomó Temp V, una variante del Compound V que te da superpoderes por 24 horas, para poder llevar la pelea directamente a Homelander y Vought. Pero hacerlo tuvo un costo devastador: ahora tiene menos de un año de vida.",
+        interval: 10000
+    },
+    {
+        src: "images/characters/Annie_January.png",
+        alt: "Foto de Annie January",
+        logo: "images/black_logo.png",
+        title: "Annie January / Starlight",
+        description: "Una ex-heroína de un pequeño pueblo que soñaba con unirse a Los Siete, Annie cumplió su deseo solo para ver de primera mano lo corruptos que Vought y sus Super realmente son. Intentó luchar contra esa corrupción desde dentro de Vought, pero pronto se dio cuenta de que era imposible. Renunció públicamente a Los Siete y se unió a su novio, Hughie, como miembro oficial de Los Muchachos.",
+        interval: 2000
+    },
+    {
+        src: "images/characters/Kimiko.png",
+        alt: "Foto de Kimiko",
+        logo: "images/black_logo.png",
+        title: "Kimiko",
+        description: "Kimiko fue secuestrada de niña y obligada a ser una niña soldado, una experiencia que la dejó muda. Después de ser inyectada a la fuerza con Compound V por el grupo terrorista Shining Light, se convirtió en una poderosa Supe con la capacidad de curarse rápidamente de incluso las heridas más extremas. Después de ser golpeada por la explosión radioactiva de Soldier Boy y perder sus superpoderes por ello, Kimiko al principio estaba encantada de ser una humana normal.",
+        interval: 10000
+    },
+    {
+        src: "images/characters/MothersMilk.png",
+        alt: "Foto de MothersMilk",
+        logo: "images/black_logo.png",
+        title: "MothersMilk",
+        description: "Exmarine y brillante táctico, Butcher depende de M.M. para llevar a cabo los planes y mantener al equipo unido. M.M. ha odiado a los Super desde una edad temprana, después de que Soldier Boy matara a varios miembros de su familia. Ese trauma ahora se manifiesta como un comportamiento obsesivo-compulsivo y una diligencia que resulta valiosa en la lucha contra Vought.",
+        interval: 10000
+    }
+];
+
+const carouselSevenData = [
+    {
+        src: "images/characters/Homelander.png",
+        alt: "Foto de Homelander",
+        logo: "images/seven_logo.png",
+        title: "Homelander",
+        description: "El Supe más poderoso de la Tierra y el líder de Los Siete, el principal equipo de superhéroes del mundo, Homelander posee una gama de poderes que incluye superfuerza, vuelo y la capacidad de disparar rayos láser desde sus ojos. Pero detrás de la máscara heroica se encuentra un hombre profundamente perturbado e inestable que se considera un dios, superior a los humanos y a todos los demás Super.",
+        interval: 10000
+    },
+    {
+        src: "images/characters/Firecracker.png",
+        alt: "Foto de Firecracker",
+        logo: "images/seven_logo.png",
+        title: "Firecracker",
+        description: "También se une a Los Siete Firecracker, una Supe de Daytona Beach, Florida, con una venganza personal contra Starlight.",
+        interval: 10000
+    },
+    {
+        src: "images/characters/Black_Noir.png",
+        alt: "Foto de Black Noir",
+        logo: "images/seven_logo.png",
+        title: "Black Noir",
+        description: "Un maestro del sigilo, Black Noir es un Supe con una larga historia en Vought. Anteriormente miembro del equipo de superhéroes de Soldier Boy, Payback, desde entonces se ha unido a Los Siete y se ha convertido en uno de los confidentes más cercanos de Homelander.",
+        interval: 10000
+    }
+];
+
+const carouselVoughtData = [
+    {
+        src: "images/characters/Ashley.png",
+        alt: "Foto de Ashley",
+        logo: "images/vought_logo.png",
+        title: "Ashley Barrett",
+        description: "Originalmente publicista de Vought, Ashley escaló rápidamente en la jerarquía corporativa para convertirse en CEO de la empresa. Sin embargo, recibió su ascenso después de que Homelander asesinara a su predecesora, Madelyn Stillwell, y recomendara a Ashley para el puesto. Aterrorizada por Homelander, Ashley ha sido leal hasta el extremo.",
+        interval: 10000
+    },
+    {
+        src: "images/characters/Ryan.png",
+        alt: "Foto de Ryan",
+        logo: "images/vought_logo.png",
+        title: "Ryan Butcher",
+        description: "Ryan es el hijo de Homelander y el primer Supe nacido naturalmente, con poderes que igualan a los de su padre. Después de matar accidentalmente a su madre y esposa de Butcher, Becca, Ryan fue acogido por Grace Mallory para ser criado bajo protección de la CIA. Pero tras distanciarse de Butcher, eligió ocupar su lugar al lado de su padre.",
+        interval: 10000
+    }
+];
+
+const carouselElectoralData = [
+    {
+        src: "images/characters/Victoria_Neuman.png",
+        alt: "Foto de Victoria Neuman",
+        logo: "images/gob.png",
+        title: "Victoria Neuman",
+        description: "Victoria Neuman es una política en ascenso que fundó la Oficina Federal de Asuntos Superhumanos para responsabilizar a los Super por sus acciones. Victoria y Hughie se hicieron amigos cercanos mientras trabajaban juntos en la OFAS durante un año... hasta que Hughie descubrió que Neuman era la misteriosa Super que hacía explotar cabezas y fue responsable del ataque al Congreso.",
+        interval: 10000
+    },
+    {
+        src: "images/characters/Robert_Singer.png",
+        alt: "Foto de Robert Singer",
+        logo: "images/gob.png",
+        title: "Robert Singer",
+        description: "Anteriormente el curtido Secretario de Defensa con rencor hacia Vought, 'Dakota Bob' Singer recientemente anunció su candidatura para Presidente de los Estados Unidos. Pero al seleccionar a Victoria Neuman como su compañera de fórmula, sin saberlo, puso en peligro mortal tanto a sí mismo como al país.",
+        interval: 10000
+    }
+];
--- js/reviewsData.js
+++ js/reviewsData.js
@@ -1,72 +1,162 @@
 const reviews = [
     {
-      title: "Un Vistazo Intenso a 'The Boys'",
+      title: "Impactante",
+      date: "Publicado el 1 de Julio, 2024",
+      content: [
+        "\"The Boys\" redefine el género de superhéroes con su brutalidad y crítica social incisiva.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Sorpresiva",
+      date: "Publicado el 30 de Junio, 2024",
+      content: [
+        "Karl Urban brilla en un elenco estelar. Una serie que mantiene al borde del asiento.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Increíble",
+      date: "Publicado el 29 de Junio, 2024",
+      content: [
+        "Crítica feroz al poder y la corrupción, con escenas de acción impresionantes.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Adictiva",
+      date: "Publicado el 28 de Junio, 2024",
+      content: [
+        "Guion afilado y personajes complejos. Una serie que te deja con ganas de más.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Provocadora",
+      date: "Publicado el 27 de Junio, 2024",
+      content: [
+        "Sátira brillante de la sociedad moderna. Efectos especiales y acción de primera.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Sorprendente",
+      date: "Publicado el 26 de Junio, 2024",
+      content: [
+        "Una crítica aguda al poder corporativo y los superhéroes comerciales.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Brutal",
+      date: "Publicado el 25 de Junio, 2024",
+      content: [
+        "Actuaciones memorables de Antony Starr y Karl Urban. Guion ingenioso.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Fascinante",
+      date: "Publicado el 24 de Junio, 2024",
+      content: [
+        "Personajes meticulosamente desarrollados. Una montaña rusa emocional.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Oscura",
+      date: "Publicado el 23 de Junio, 2024",
+      content: [
+        "Desde el primer episodio, te sumerge en acción y drama intensos.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Tensa",
+      date: "Publicado el 22 de Junio, 2024",
+      content: [
+        "Desarrollo de personajes único. Mantiene la tensión y el suspenso.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Innovadora",
+      date: "Publicado el 21 de Junio, 2024",
+      content: [
+        "Un viaje oscuro y provocador que redefine los superhéroes.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Emocionante",
+      date: "Publicado el 20 de Junio, 2024",
+      content: [
+        "Actuaciones de primer nivel y química palpable entre los personajes.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Audaz",
+      date: "Publicado el 19 de Junio, 2024",
+      content: [
+        "Una serie que no deja indiferente a nadie con su narrativa y personajes complejos.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Inolvidable",
+      date: "Publicado el 18 de Junio, 2024",
+      content: [
+        "Dirección impecable y giros inesperados. Una serie destacada.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Reflexiva",
+      date: "Publicado el 17 de Junio, 2024",
+      content: [
+        "Aborda temas difíciles con brutal franqueza. Cuestiona todo sobre superhéroes.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Extraordinaria",
+      date: "Publicado el 16 de Junio, 2024",
+      content: [
+        "Humor negro, acción intensa y crítica social en una serie única.",
+      ],
+      authorImage: "images/persona.png"
+    },
+    {
+      title: "Enganchante",
       date: "Publicado el 15 de Junio, 2024",
       content: [
-        "\"The Boys\" es una serie que redefine el género de superhéroes. Con una mezcla de humor negro, acción intensa y crítica social, este show nos presenta un mundo donde los héroes no siempre son los buenos.",
-        "La actuación de Karl Urban como Billy Butcher es sobresaliente, y la dinámica entre los personajes mantiene a los espectadores enganchados episodio tras episodio.",
-        "Si buscas una serie que desafíe las convenciones y te mantenga al borde de tu asiento, \"The Boys\" es una elección obligada."
+        "Dinámica entre personajes que mantiene a los espectadores enganchados.",
       ],
       authorImage: "images/persona.png"
     },
     {
-      title: "Impactante y Brutal",
-      date: "Publicado el 10 de Junio, 2024",
+      title: "Impactante",
+      date: "Publicado el 14 de Junio, 2024",
       content: [
-        "\"The Boys\" nos lleva a un mundo donde los superhéroes son corruptos y brutales. La serie ofrece una crítica feroz al poder y la corrupción, con escenas de acción que no tienen desperdicio.",
-        "El guion es inteligente y afilado, y cada episodio te deja con ganas de más. Es una serie que no teme ir más allá y mostrar el lado oscuro del heroísmo."
+        "Revela el lado oscuro del heroísmo con escenas de acción impresionantes.",
       ],
       authorImage: "images/persona.png"
     },
     {
-      title: "Una Crítica a la Sociedad Moderna",
-      date: "Publicado el 5 de Junio, 2024",
+      title: "Sofisticada",
+      date: "Publicado el 13 de Junio, 2024",
       content: [
-        "\"The Boys\" no solo es una serie de acción, sino también una aguda crítica a la sociedad y al poder corporativo. La manera en que se retratan los superhéroes como productos comerciales es una sátira brillante.",
-        "Los efectos especiales son impresionantes y las escenas de acción están perfectamente coreografiadas, haciendo que cada episodio sea una experiencia emocionante.",
-        "Es una serie que te hará reflexionar sobre el verdadero significado del poder y la responsabilidad."
+        "Una crítica brillante al poder corporativo y la sociedad moderna.",
       ],
       authorImage: "images/persona.png"
     },
     {
-      title: "Un Elenco Estelar y Guion Impecable",
-      date: "Publicado el 1 de Junio, 2024",
+      title: "Asombrosa",
+      date: "Publicado el 12 de Junio, 2024",
       content: [
-        "El elenco de \"The Boys\" es simplemente magnífico. Antony Starr como Homelander y Karl Urban como Billy Butcher entregan actuaciones memorables que son tanto aterradoras como fascinantes.",
-        "El guion es ingenioso y mantiene a los espectadores constantemente sorprendidos con sus giros y revelaciones. Cada personaje está meticulosamente desarrollado, lo que añade profundidad a la historia.",
-        "Es una serie que no teme explorar temas oscuros y controvertidos, haciendo que cada episodio sea una montaña rusa emocional."
-      ],
-      authorImage: "images/persona.png"
-    },
-    {
-      title: "Acción y Drama en su Máxima Expresión",
-      date: "Publicado el 28 de Mayo, 2024",
-      content: [
-        "Desde el primer episodio, \"The Boys\" te sumerge en un mundo lleno de acción y drama. Las secuencias de pelea son intensas y gráficas, mostrando la brutalidad de una manera realista y cruda.",
-        "El desarrollo de los personajes es uno de los puntos fuertes de la serie. Cada miembro de \"The Boys\" tiene una historia única que se entrelaza perfectamente con la trama principal.",
-        "Es una serie que sabe cómo mantener la tensión y el suspenso, haciendo que sea difícil dejar de verla."
-      ],
-      authorImage: "images/persona.png"
-    },
-    {
-      title: "Un Viaje Oscuro y Provocador",
-      date: "Publicado el 20 de Mayo, 2024",
-      content: [
-        "\"The Boys\" es un viaje oscuro y provocador que desafía las expectativas tradicionales de las series de superhéroes. Con su enfoque en la corrupción y el abuso de poder, la serie ofrece una perspectiva refrescante y realista.",
-        "Las actuaciones son de primer nivel, y la química entre los personajes principales es palpable. La serie logra equilibrar perfectamente el humor negro con momentos de verdadero terror y tensión.",
-        "Es una obra maestra moderna que redefine lo que significa ser un superhéroe en la era contemporánea."
-      ],
-      authorImage: "images/persona.png"
-    },
-    {
-      title: "Una Serie Que No Deja Indiferente",
-      date: "Publicado el 15 de Mayo, 2024",
-      content: [
-        "\"The Boys\" es una serie que no deja indiferente a nadie. Con su narrativa audaz y sus personajes complejos, logra capturar la atención del espectador desde el primer minuto.",
-        "La dirección es impecable, y cada episodio está lleno de sorpresas y giros inesperados. La serie aborda temas difíciles con una franqueza brutal, lo que la hace destacarse en el panorama televisivo actual.",
-        "Si buscas una serie que te haga cuestionar todo lo que sabes sobre los superhéroes, \"The Boys\" es la elección perfecta."
+        "Personajes meticulosamente desarrollados en una serie fascinante.",
       ],
       authorImage: "images/persona.png"
     }
   ];
-  
